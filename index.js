f63e6a0b372de8e2304fda2b4549bf9ffbd550f6
Kyle Conlon <kyleconlon13@gmail.com>
2019-05-15 18:30:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function currentLine (katzDeliLine) {
   if (katzDeli.length === 0) {
     return "The line is currently empty.";
   } else {
-    return `The line is currently:` + katzDeliLine;
+    return (`The line is currently:` + katzDeliLine);
   }
 }
 
