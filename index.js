a76ef0bf52143acac0b290d13921be697357b91e
Kyle Conlon <kyleconlon13@gmail.com>
2019-05-15 18:06:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,2 @@
-var katzDeliLine = [];
-
-function takeANumber (katzDeliLine, name) {
-  katzDeliLine.push(katzDeliLine.length + 1);
-  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
-}
-
-function nowServing (katzDeliLine) {
-  let i = 0;
-  while (i < katzDeliLine.length) {
-    i++
-  }
-  if (katzDeliLine.length > 0) {
-    katzDeliLine.unshift(katzDeliLine.length - [i])
-    return (`Currently serving ${katzDeliLine}`)
-  } else {
-  return "There is nobody waiting to be served!"
-  }
-}
-
-let katzDeli = [];
-
-function currentLine (katzDeliLine) {
-}
-
 
 
