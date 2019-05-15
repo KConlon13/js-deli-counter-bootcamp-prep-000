caa0101a443177bc7b51d21a76f2f7c653f929e7
Kyle Conlon <kyleconlon13@gmail.com>
2019-05-15 18:12:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var katzDeli = [];
 
 function takeANumber (katzDeli, name) {
   katzDeli.push(`${name}`);
-  return (`Welcome, ${name}. You are number ${katzDeli} in line.`);
+  return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
