9e75acd4f82c6718e2ae9b4c42d220107c225579
Kyle Conlon <kyleconlon13@gmail.com>
2019-05-15 18:14:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function takeANumber (katzDeli, name) {
   return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
 
+function nowServing (katzDeli) {
+  //while (katzDeli.length>0) {
+    // i++;
+  }
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!"
+  } return 
+}
