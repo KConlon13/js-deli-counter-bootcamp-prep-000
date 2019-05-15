da8a599d8319bfba06bc92c93e60ffb76022a5e2
Kyle Conlon <kyleconlon13@gmail.com>
2019-05-15 18:25:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ function nowServing (katzDeli) {
     return "There is nobody waiting to be served!"
   } return `Currently serving ${katzDeli.shift()}.`;
 }
+
+var katzDeliLine = [];
+
+function currentLine (katzDeliLine) {
+  for (i=0; i<katzDeli.length; i++) {
+    katzDeliLine.push([i+1] + katzDeli[i]);
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty."
+  } return `The line is currently:` + katzDeliLine;
+}
+
+
